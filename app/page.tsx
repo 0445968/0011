fb6549006893d870e6175f1537d9600dbc89215c
0445968 <crafterkite@gmail.com>
2026-08-06 16:23:31 -0500

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from '@/components/portfolio/Hero';
 import { About } from '@/components/portfolio/About';
 import { Projects } from '@/components/portfolio/Projects';
-import { ResourceLibraryPreview } from '@/components/portfolio/ResourceLibraryPreview';
+import { ResourceLibraryPreview } from '@/components/portfolio/resources/ResourceLibraryPreview';
 import { CaseStudy } from '@/components/portfolio/CaseStudy';
 import { FeaturedCaseStudies } from '@/components/portfolio/FeaturedCaseStudies'
 import { ServicesCarousel } from '@/components/portfolio/services/ServicesCarousel';
