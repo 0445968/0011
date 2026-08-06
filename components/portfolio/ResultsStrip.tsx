6d532b0b52725ad0ebe714f165739f9162475e37
0445968 <crafterkite@gmail.com>
2026-08-06 15:04:24 -0500

    Updated ResultsStrip.tsx

--- components/portfolio/ResultsStrip.tsx
+++ components/portfolio/ResultsStrip.tsx
@@ -24,7 +24,7 @@ export function ResultsStrip({
           "
         >
 
-          <p className="font-serif text-2xl font-semibold text-[#A4D62D]">
+          <p className="font-serif text-2xl font-semibold text-[#71D62D]">
             {result.value}
           </p>
 
