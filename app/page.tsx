5f63c741820396f16136a5c9ca88c30bd03a4cdc
0445968 <crafterkite@gmail.com>
2026-08-06 10:49:19 -0500

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <>
       <Hero />
       <About />
-      <ServicesMarquee />
+      <ServicesCarousel />
       <Services />
       <FeaturedCaseStudies />
       <ResourceLibraryPreview />
