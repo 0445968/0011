8b658b46754dffd817ef99f573a2ad0b98630974
0445968 <crafterkite@gmail.com>
2026-08-14 12:20:46 -0500

    Updated About.tsx

---
+++ components/home/About.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { AboutCustomers } from './about/AboutCustomers';
+import { AboutFeature } from './about/AboutFeature';
+import { AboutIntro } from './about/AboutIntro';
+
+export function About() {
+  return (
+    <section
+      id="about"
+      className="
+        relative
+        overflow-hidden
+        bg-background
+        py-20
+        sm:py-28
+        lg:py-36
+      "
+    >
+      <div className="container-page">
+        <AboutCustomers />
+        <AboutIntro />
+        <AboutFeature />
+      </div>
+    </section>
+  );
+}
